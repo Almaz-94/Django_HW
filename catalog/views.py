ed4bc3d4e56627ae9f8fe3476b18deb34eb011c4
Almaz <almaz28-10@mail.ru>
2023-10-12 18:48:02 +0300

    Added POST

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 # Create your views here.
 
-
-def contacts(request):
-    return render(request, 'catalog/contacts.html')
-
 def home(request):
     return render(request, 'catalog/home.html')
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone,'\n',message)
+    return render(request, 'catalog/contacts.html')
+
